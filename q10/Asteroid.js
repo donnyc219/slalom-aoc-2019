6f9f40282f508067d447a217efa6eb2eb9bba92e
Donny Cheng <donccl@gmail.com>
2019-12-15 16:46:08 -0500

    q10 part2 not finished

--- q10/Asteroid.js
+++ q10/Asteroid.js
@@ -38,6 +38,17 @@ class Asteroid {
         if (this.x > asteroid.getX())   return true;
         return false;
     }
+
+    getPoints(){
+        return {
+            x: this.x,
+            y: this.y
+        };
+    }
+
+    distanceFrom(asteroid){
+        return Math.sqrt(Math.pow(this.x - asteroid.getX(),2) + Math.pow(this.y - asteroid.getY(),2));
+    }
 }
 
 
--- q10/main.js
+++ q10/main.js
@@ -9,11 +9,51 @@ const go = async () => {
     try {
         var asteroids = await getAListOfAsteroids();
 
-        part1(asteroids);
+        // part1(asteroids);
+        // part2(asteroids);
+
+        ass = new Asteroid(3, 4);
+        var asteroid;
+        for (var i in asteroids) {
+            asteroid = asteroids[i];
+            var d = ass.distanceFrom(asteroid);
+            console.log(`${asteroid.getID()}: ${d}`);
+        }
 
     } catch (error) {
         console.log(error);
     }
+}
+
+const getAngleInDegree = (center, point) => {
+    var {x, y} = point;
+    x -= center.x;
+    y -= center.y;
+
+    var rad = Math.atan(x/y);
+    var degree = toDegree(rad);
+    if (y<0)    return (180 + degree);
+    else if (x<0 && y>=0)    return (360 + degree);
+
+    return degree;
+}
+
+const toDegree = (radian) => {
+    return radian * (180/Math.PI);
+}
+
+const part2 = (asteroids) => {
+    var center = {
+        x: 0,
+        y: 0
+    }
+    var point = {
+        x: 100,
+        y: 0
+    }
+
+    var n = getAngleInDegree(center, point);
+    console.log(n);
 }
 
 const part1 = (asteroids) => {
