b2dbc2abbe854138ae0f228593a9c9dc35e77356
Donny Cheng <donccl@gmail.com>
2019-12-20 22:44:16 -0500

    finished q5 part1

--- q5/main.js
+++ q5/main.js
@@ -2,11 +2,18 @@
 
 var data = "3,225,1,225,6,6,1100,1,238,225,104,0,1101,65,73,225,1101,37,7,225,1101,42,58,225,1102,62,44,224,101,-2728,224,224,4,224,102,8,223,223,101,6,224,224,1,223,224,223,1,69,126,224,101,-92,224,224,4,224,1002,223,8,223,101,7,224,224,1,223,224,223,1102,41,84,225,1001,22,92,224,101,-150,224,224,4,224,102,8,223,223,101,3,224,224,1,224,223,223,1101,80,65,225,1101,32,13,224,101,-45,224,224,4,224,102,8,223,223,101,1,224,224,1,224,223,223,1101,21,18,225,1102,5,51,225,2,17,14,224,1001,224,-2701,224,4,224,1002,223,8,223,101,4,224,224,1,223,224,223,101,68,95,224,101,-148,224,224,4,224,1002,223,8,223,101,1,224,224,1,223,224,223,1102,12,22,225,102,58,173,224,1001,224,-696,224,4,224,1002,223,8,223,1001,224,6,224,1,223,224,223,1002,121,62,224,1001,224,-1302,224,4,224,1002,223,8,223,101,4,224,224,1,223,224,223,4,223,99,0,0,0,677,0,0,0,0,0,0,0,0,0,0,0,1105,0,99999,1105,227,247,1105,1,99999,1005,227,99999,1005,0,256,1105,1,99999,1106,227,99999,1106,0,265,1105,1,99999,1006,0,99999,1006,227,274,1105,1,99999,1105,1,280,1105,1,99999,1,225,225,225,1101,294,0,0,105,1,0,1105,1,99999,1106,0,300,1105,1,99999,1,225,225,225,1101,314,0,0,106,0,0,1105,1,99999,1008,226,677,224,102,2,223,223,1005,224,329,1001,223,1,223,7,677,226,224,102,2,223,223,1006,224,344,1001,223,1,223,1007,226,677,224,1002,223,2,223,1006,224,359,1001,223,1,223,1007,677,677,224,102,2,223,223,1005,224,374,1001,223,1,223,108,677,677,224,102,2,223,223,1006,224,389,101,1,223,223,8,226,677,224,102,2,223,223,1005,224,404,101,1,223,223,7,226,677,224,1002,223,2,223,1005,224,419,101,1,223,223,8,677,226,224,1002,223,2,223,1005,224,434,101,1,223,223,107,677,677,224,1002,223,2,223,1006,224,449,101,1,223,223,7,677,677,224,1002,223,2,223,1006,224,464,101,1,223,223,1107,226,226,224,102,2,223,223,1006,224,479,1001,223,1,223,1007,226,226,224,102,2,223,223,1006,224,494,101,1,223,223,108,226,677,224,1002,223,2,223,1006,224,509,101,1,223,223,1108,226,677,224,102,2,223,223,1006,224,524,1001,223,1,223,1008,226,226,224,1002,223,2,223,1005,224,539,101,1,223,223,107,226,226,224,102,2,223,223,1006,224,554,101,1,223,223,8,677,677,224,102,2,223,223,1005,224,569,101,1,223,223,107,226,677,224,102,2,223,223,1005,224,584,101,1,223,223,1108,226,226,224,1002,223,2,223,1005,224,599,1001,223,1,223,1008,677,677,224,1002,223,2,223,1005,224,614,101,1,223,223,1107,226,677,224,102,2,223,223,1005,224,629,101,1,223,223,1108,677,226,224,1002,223,2,223,1005,224,644,1001,223,1,223,1107,677,226,224,1002,223,2,223,1006,224,659,1001,223,1,223,108,226,226,224,102,2,223,223,1006,224,674,101,1,223,223,4,223,99,226";
 
-
-
+const upgradeArray = (arr) => {
+    arr.getValueOfAddressOfIndex = function(index) {
+        var n = this[index];
+        return this[n];
+    }
+}
 const part1 = () => {
 
     var arr = data.split(",");
+    upgradeArray(arr);
+
+    console.log(`arr length: ${arr.length}`);
     var input = 1;
     var output;
 
@@ -16,11 +23,11 @@ const part1 = () => {
         digit = arr[ptr];
         switch (digit) {
             case "1":
-                arr = handleAddition(arr, ptr);
+                arr = handleNormalCase(arr, ptr, add);
                 ptr += 4;
             break;
             case "2":
-                arr = handleMultiply(arr, ptr);
+                arr = handleNormalCase(arr, ptr, multiply);
                 ptr += 4;
             break;
             case "3":
@@ -28,12 +35,13 @@ const part1 = () => {
                 ptr += 2;
             break;
             case "4":
-                output = outputValueAtAddress(arr, parseInt(ptr)+1);
+                output = arr.getValueOfAddressOfIndex(ptr+1);
                 console.log(`output: ${output}`);
                 ptr += 2;
             break;
             case "99":
                 console.log("halt!");
+                console.log(`pointer = ${ptr}`);
                 return ;
             default:
                 var res = handleInstruction(arr, ptr);
@@ -76,10 +84,8 @@ const handleInstruction = (arr, ptr) => {
         case 2:
             val1 = getValueWithMode(arr, mode1, ptr+1);
             val2 = getValueWithMode(arr, mode2, ptr+2);
-            console.log(`val1: ${val1}, val2: ${val2}`);
             newValue = (opCode==1)? add(val1, val2): multiply(val1, val2);
-            var saveToAddress = getValueInAddress(arr, ptr+3);
-            console.log(`save to addr: ${newValue}`);
+            var saveToAddress = arr[ptr+3];
             arr[saveToAddress] = newValue;
             return {
                 arr: arr,
@@ -88,7 +94,7 @@ const handleInstruction = (arr, ptr) => {
         case 3:
             throw "Requesting an input?";
         case 4:
-            val1 = getValueInAddress(arr, ptr+1);
+            val1 = arr[ptr+1];
             console.log(val1);
             return {
                 arr: arr,
@@ -96,8 +102,6 @@ const handleInstruction = (arr, ptr) => {
             }
     }
 
-    console.log(arr[ptr]);
-    console.log(ptr);
     throw "Something wrong here";
 }
 
@@ -112,43 +116,34 @@ const splitInstruction = (instruction) => {
         parameter2 = 1;
     }
 
-    return {
+    var res = {
         opCode: opCode,
         mode1: parameter1,
         mode2: parameter2
     }
+    // console.log(instruction);
+    // console.log(`splitInstruction: ${JSON.stringify(res)}`);
+
+    return res;
 }
 
 const getLastTwoDigit = (instruction) => {
+    instruction = instruction.toString();
     if (instruction.length<2)   throw "Instruction too short. Cannot get the last 2 digits";
-    return parseInt(instruction.toString().substring(instruction.length-2));
+    return parseInt(instruction.substring(instruction.length-2));
 }
 
-const handleAddition = (arr, ptr) => {
-    var val1 = getValueInAddress(arr, ptr+1);
-    val1 = arr[val1];
-    var val2 = getValueInAddress(arr, ptr+2);
-    val2 = arr[val2];
-    var saveToAddress = getValueInAddress(arr, parseInt(ptr)+3)
-    var newValue = add(val1, val2);
-    arr[saveToAddress] = newValue;
-    console.log(`${saveToAddress}, ${val1}, ${val2}, ${ptr}`);
-    return arr;
-}
-
-const handleMultiply = (arr, ptr) => {
-    var val1 = getValueInAddress(arr, parseInt(ptr)+1);
-    val1 = arr[val1];
-    var val2 = getValueInAddress(arr, parseInt(ptr)+2);
-    val2 = arr[val2];
-    var saveToAddress = getValueInAddress(arr, parseInt(ptr)+3)
-    var newValue = multiply(val1, val2);
+const handleNormalCase = (arr, ptr, handler) => {
+    var val1 = arr.getValueOfAddressOfIndex(ptr+1);
+    var val2 = arr.getValueOfAddressOfIndex(ptr+2);
+    var newValue = handler(val1, val2);
+    var saveToAddress = arr[ptr+3];
     arr[saveToAddress] = newValue;
     return arr;
 }
 
 const handleInput = (arr, input, ptr) => {
-    var saveToAddress = getValueInAddress(arr, parseInt(ptr)+1);
+    var saveToAddress = arr[ptr+1];
     arr[saveToAddress] = input;
     return arr;
 }
@@ -158,15 +153,5 @@ const takeInputAndSaveToPosition = (arr, input, saveToPosition) => {
     return arr;
 }
 
-const outputValueAtAddress = (arr, outputAddress) => {
-    return arr[outputAddress];
-}
-
-const getValueInAddress = (arr, index) => {
-    return arr[parseInt(index)];
-}
-// var arr = data.split(",");
-// var n = getValueInAddress(arr, 6);
-// var n2 = getValueInAddress(arr, 225);
 
 part1();
