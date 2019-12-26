ebdcc56de2ca8cda0fc871477b9f85b3b5bea2fc
Donny Cheng <donccl@gmail.com>
2019-12-25 22:05:39 -0500

    getting closer maybe

--- q7/main.js
+++ q7/main.js
@@ -1,18 +1,18 @@
 var Amplifier = require("./Amplifier");
 
-// var set = new Set();
-// set.add("5");
-// set.add("6");
-// set.add("7");
-// set.add("8");
-// set.add("9");
-
 var set = new Set();
-set.add("0");
-set.add("1");
-set.add("2");
-set.add("3");
-set.add("4");
+set.add("5");
+set.add("6");
+set.add("7");
+set.add("8");
+set.add("9");
+
+// var set = new Set();
+// set.add("0");
+// set.add("1");
+// set.add("2");
+// set.add("3");
+// set.add("4");
 
 
 const getPossibleCombinationsStartingWith = (numStr, set) => {
@@ -28,11 +28,11 @@ const getPossibleCombinationsStartingWith = (numStr, set) => {
     return res;
 }
 const getPossibleCombinations = (set) => {
-    var arr = getPossibleCombinationsStartingWith("0", set);
-    arr = arr.concat(getPossibleCombinationsStartingWith("1", set));
-    arr = arr.concat(getPossibleCombinationsStartingWith("2", set));
-    arr = arr.concat(getPossibleCombinationsStartingWith("3", set));
-    arr = arr.concat(getPossibleCombinationsStartingWith("4", set));
+    var arr = getPossibleCombinationsStartingWith("5", set);
+    arr = arr.concat(getPossibleCombinationsStartingWith("6", set));
+    arr = arr.concat(getPossibleCombinationsStartingWith("7", set));
+    arr = arr.concat(getPossibleCombinationsStartingWith("8", set));
+    arr = arr.concat(getPossibleCombinationsStartingWith("9", set));
     return arr;
 }
 
@@ -59,6 +59,38 @@ const getOutputWithNumberString = (numStr, amplifiers) => {
     return prevOutput;
 }
 
+const getOutputWithNumberStringV2 = (numStr, amplifiers) => {
+    var prevOutput = 0;
+    var amplifier;
+
+    while (!areAllAmplifiersHalted()) {
+        for (var i=0; i<numStr.length; i++) {
+            amplifier = amplifiers[i];
+            // console.log(`numStr: ${numStr}, i: ${i}, ${amplifier}`);
+            prevOutput = amplifier.runWithInput([numStr[i], prevOutput]);
+            // console.log(`prevOutput: ${prevOutput}`);
+        }
+        
+        // for (var i in numStr) {
+        //     console.log(`i: ${i}`);
+        //     amplifier = amplifiers[i];
+        //     prevOutput = amplifier.runWithInput([numStr[i], prevOutput]);
+        //     // console.log(`prevOutput: ${prevOutput}`);
+        // }
+    }
+
+    return prevOutput;
+}
+
+const areAllAmplifiersHalted = () => {
+    var amp;
+    for (var i in amplifiers){
+        amp = amplifiers[i];
+        if (!amp.isAmplifierHalted())   return false;
+    }
+    return true;
+}
+
 const part1 = () => {
     var arr = getPossibleCombinations(set);
     var numStr;
@@ -66,9 +98,9 @@ const part1 = () => {
     var resArray = [];
     for (var i in arr) {
         numStr = arr[i];
-        output = getOutputWithNumberString(numStr, amplifiers);
+        output = getOutputWithNumberStringV2(numStr, amplifiers);
         resArray.push(output);
-        resetAmplifier();
+        // resetAmplifier();
     }
 
 
@@ -89,8 +121,24 @@ amplifiers.push(new Amplifier("D"));
 amplifiers.push(new Amplifier("E"));
 
 
-// var amp = amplifiers[0];
-// amp.runWithInput([0, 0]);
+var ampA = amplifiers[0];
+var ampB = amplifiers[1];
+var ampC = amplifiers[2];
+var ampD = amplifiers[3];
+var ampE = amplifiers[4];
 
-var kk = part1();
-console.log(kk);
+var n = ampA.runWithInput([0, 9]);
+n = ampB.runWithInput([n, 8]);
+n = ampC.runWithInput([n, 7]);
+n = ampD.runWithInput([n, 6]);
+n = ampE.runWithInput([n, 5]);
+
+n = ampA.runWithInput([n]);
+// n = ampB.runWithInput([n]);
+// n = ampC.runWithInput([n]);
+// n = ampD.runWithInput([n]);
+// n = ampE.runWithInput([n]);
+
+console.log(n);
+// var kk = part1();
+// console.log(kk);
