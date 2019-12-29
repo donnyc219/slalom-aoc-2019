4674fa7455318e5e54a1c9568faf1d005c76b00a
Donny Cheng <donccl@gmail.com>
2019-12-29 16:23:05 -0500

    some changes in test

--- test.js
+++ test.js
@@ -11,26 +11,5 @@ const resetValues = () => {
     }
 }
 
-var counter = 12;
-var obj = {counter};
-// console.log(obj);
-
-// var {n, b, c} = resetValues();
-
-// console.log(n);
-// console.log(b);
-// console.log(c);
-// {n, b, c, counter} = resetValues();
-
-
-var arr = [1,1,2,3,7,6];
-
-// const hi = 
-arr.hi = function(index) {
-    return this[index+3];
-}
-
-var n = arr.hi(1);
-console.log(n)
 
 
