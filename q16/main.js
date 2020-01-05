cadc298016316e95d0e2208f4886e6d7f0512f05
Donny Cheng <donccl@gmail.com>
2020-01-05 17:32:49 -0500

    misunderstood the question. Wrote wrong logic for buildBasePatternMaskForDigit() Now fixed buildBasePatternMaskForDigit()

--- q16/main.js
+++ q16/main.js
@@ -12,18 +12,18 @@ function incrementPointerWithinBound(ptr, notMoreThanLength) {
 }
 
 function buildBasePatternMaskForDigit(calDigit, inputArray, basePatternArray) {
-    let repeat = calDigit;
+    let repeat = parseInt(calDigit)+1;
     let res = [];
     let ptr = 0;
     let value;
-    while (res.length < inputArray.length) {
+    
+    while (res.length < (inputArray.length+1)) {
         value = basePatternArray[ptr];
         res = pushValueNTimes(res, value, repeat);
 
-        if (ptr==0) repeat++;
         ptr = incrementPointerWithinBound(ptr, basePatternArray.length);
     }
-    res = res.slice(0, inputArray.length);
+    res = res.slice(1, inputArray.length+1);
     return res;
 }
 
@@ -35,7 +35,7 @@ function buildBasePatternMaskForAllInput(inputArray, basePatternArray) {
     return res;
 }
 
-let inputArray = [1,2,3,4,5,6,7,8];
+let inputArray = [1,2,3,4,5,6,7,8,9,10,11,12,13];
 let basePatternArray = [0,1,0,-1];
 
 let n = buildBasePatternMaskForAllInput(inputArray, basePatternArray);
