d2a1f790e9a8e3769f544e74d74f9348c0380ede
Donny Cheng <donccl@gmail.com>
2020-01-05 20:18:24 -0500

    q12 part1 is done

--- q16/main.js
+++ q16/main.js
@@ -35,8 +35,54 @@ function buildBasePatternMaskForAllInput(inputArray, basePatternArray) {
     return res;
 }
 
-let inputArray = [1,2,3,4,5,6,7,8,9,10,11,12,13];
-let basePatternArray = [0,1,0,-1];
+function getLastDigitOf(n) {
+    return Math.abs(parseInt(n)%10);
+}
 
-let n = buildBasePatternMaskForAllInput(inputArray, basePatternArray);
+function getDigitNWithMask(mask, inputArray, n) {
+    let submask = mask[n];
+    if (submask.length!=inputArray.length)  throw "length of submask and inputArray do not match!";
+
+    let sum = 0;
+    let digit, maskDigit;
+    for (let i in inputArray) {
+        digit = parseInt(inputArray[i]);
+        maskDigit = submask[i];
+        sum += maskDigit * digit;
+    }
+    return getLastDigitOf(sum);
+}
+
+function operatePhase(inputArray, mask) {
+    let res = "";
+    for (let i=0; i<inputArray.length; i++) {
+        res += getDigitNWithMask(mask, inputArray, i).toString();
+    }
+    return res;
+}
+
+function getFirstNDigitOf(num, n) {
+    let numStr = num.toString();
+    return numStr.substring(0, n);
+}
+
+function part1() {
+    // let inputArray = "59762574510031092870627555978901048140761858379740610694074091049186715780458779281173757827279664853239780029412670100985236587608814782710381775353184676765362101185238452198186925468994552552398595814359309282056989047272499461615390684945613327635342384979527937787179298170470398889777345335944061895986118963644324482739546009761011573063020753536341827987918039441655270976866933694280743472164322345885084587955296513566305016045735446107160972309130456411097870723829697443958231034895802811058095753929607703384342912790841710546106752652278155618050157828313372657706962936077252259769356590996872429312866133190813912508915591107648889331";
+    let inputArray = "12345678";
+    let basePatternArray = [0,1,0,-1];
+    let mask = buildBasePatternMaskForAllInput(inputArray, basePatternArray);
+    
+    let res = inputArray;
+    let max = 4;
+    for (let i=0; i<max; i++) {
+        res = operatePhase(res, mask);
+        // console.log(`${i}: ${res}`);
+    }
+    return getFirstNDigitOf(res, 8);
+}
+
+let n = part1();
 console.log(n);
+
+
+
